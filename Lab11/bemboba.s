d6c89cef5d9dd3962352a8f61b50feb5a7495bc1
Lucas Andre Guimarães <145869842+Lc-Andre@users.noreply.github.com>
2026-06-09 05:43:33 +0000

    Lab11

---
+++ Lab11/bemboba.s
@@ -0,0 +1,21 @@
+/*
+int bemboba (int num) {
+  int local[4];
+  int *a;
+  int i;
+
+  for (i=0,a=local;i<4;i++) {
+    *a = num;
+    a++;
+  }
+  return addl (local, 4);
+}
+
+*/
+
+.data
+
+.text
+
+.globl bemboba
+bemboba:
