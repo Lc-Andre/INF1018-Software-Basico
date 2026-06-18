c7d8588b8043f824dcb62daa6e10379c3832829f
Lucas Andre Guimarães <145869842+Lc-Andre@users.noreply.github.com>
2026-06-18 04:59:22 +0000

    Lab11

--- Lab11/bemboba.s
+++ Lab11/bemboba.s
@@ -1,21 +1,60 @@
-/*
-int bemboba (int num) {
-  int local[4];
-  int *a;
-  int i;
-
-  for (i=0,a=local;i<4;i++) {
-    *a = num;
-    a++;
-  }
-  return addl (local, 4);
-}
-
-*/
-
-.data
-
 .text
-
 .globl bemboba
 bemboba:
+
+    pushq %rbp
+    movq  %rsp, %rbp
+    subq  $32, %rsp
+
+    # salva rbx
+    movq  %rbx, -24(%rbp)
+
+    /*
+    Dicionário
+
+    edi -> num
+    ecx -> i
+    rbx -> a
+
+    local[0] -> -16(%rbp)
+    local[1] -> -12(%rbp)
+    local[2] -> -8(%rbp)
+    local[3] -> -4(%rbp)
+    */
+
+    # i = 0
+    movl $0, %ecx
+
+    # a = local
+    leaq -16(%rbp), %rbx
+
+WHILE:
+
+    cmpl $4, %ecx
+    jge FIM_WHILE
+
+    # *a = num
+    movl %edi, (%rbx)
+
+    # a++
+    addq $4, %rbx
+
+    # i++
+    incl %ecx
+
+    jmp WHILE
+
+FIM_WHILE:
+
+    # addl(local,4)
+
+    leaq -16(%rbp), %rdi
+    movl $4, %esi
+
+    call addl
+
+    # restaura rbx
+    movq -24(%rbp), %rbx
+
+    leave
+    ret
